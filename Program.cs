c18697c6b37742233e529fbe033d3b8b0e968dfc
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-15 16:59:31 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Week_2_Challenge_Lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Temperature() 
+            {
+                Console.WriteLine("What's the temperature outside?");
+                
+
+                int temp = Convert.ToInt32(Console.ReadLine());
+                switch (temp) 
+                {
+                    case <= 10:
+                        Console.WriteLine("Freezing weather");
+                        break;
+
+                    case <= 20:
+                        Console.WriteLine("Very Cold Weather");
+                        break;
+
+                    case <= 35:
+                        Console.WriteLine("Cold Weather");
+                        break;
+
+                    case <= 50:
+                        Console.WriteLine("Normal Weather");
+                        break;
+
+                    case <= 65:
+                        Console.WriteLine("Its Hot");
+                        break;
+
+                    case <= 80:
+                        Console.WriteLine("Its Very Hot");
+                        break;
+                        
+                        
+
+                }
+
+            
+            }
+
+            Temperature();
+        }
+
+    }
+}
